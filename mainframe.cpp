e250bd01c4083a042ad99125f2b1267f20fbe18c
Satalicious <enzooocf@gmail.com>
2023-05-15 22:23:40 +0200

    wip

--- mainframe.cpp
+++ mainframe.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <sstream>
 
+#include <vector>
+#include <fstream> 
+#include <chrono>
+#include <ctime>
+
 std::string to_hex(std::string s )
 {
   std::ostringstream oss;
@@ -17,14 +22,58 @@ std::string to_hex(std::string s )
   return oss.str();
 }
 
+int writeOnfile (char* filetext) {
+   std::ofstream myfile;
+   myfile.open ("data.txt", std::fstream::app);
+   myfile << filetext;
+   myfile.close();
+   return 0;
+}
+
+
+int MainFrame::LoadFromFile() {
+  //TODO:
+  std::ofstream myfile;
+  myfile.open ("data.txt", std::fstream::app);
+  
+  return 0;
+}
+
 
 void MainFrame::OnAsciiKeyDown(wxKeyEvent& key) {
-  //std::cout << key.GetUnicodeKey() << std::endl;
-  asciiInput->AppendText(key.GetUnicodeKey());
-  hexInput->AppendText(to_hex(asciiInput->GetValue().ToStdString()));
+  //The 1 specifies the length of the string to create, and the char being passed in is what to fill that string with. 
+  //So it's creating a new string of length 1, filled with the char that was created from the Unicode key.
+  std::string keyStr(1, static_cast<char>(key.GetUnicodeKey()));
+
+  // Append the Unicode character to the ASCII input field
+  asciiInput->AppendText(keyStr);
+
+  // Convert the Unicode character to hexadecimal and append it to the hex input field
+  hexInput->AppendText(to_hex(keyStr));
+}
+
+void MainFrame::OnClearButton(wxCommandEvent& event) {
+  asciiInput->Clear();
+  hexInput->Clear();
+}
+
+void MainFrame::OnSaveButton(wxCommandEvent& event) {
+  if(asciiInput->GetValue().ToStdString() != "") {
+    std::string asciiText = asciiInput->GetValue().ToStdString();
+    std::vector<std::string> asciiT;
+    asciiT.push_back(asciiText);
+    // write ascii
+    for(auto& e : asciiT) {WriteInFile(e);}
+    // write hex
+    for(auto& e : asciiT) {WriteInFile(to_hex(e));}
+  }
 
 }
 
+void MainFrame::OnLoadButton(wxCommandEvent& event) {
+  //TODO:
+  LoadFromFile();
+}
 
 MainFrame::MainFrame(const wxString &title)
     : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize,
@@ -33,9 +82,14 @@ MainFrame::MainFrame(const wxString &title)
   panel = new wxPanel(this);
 
   asciiInput = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(0, 0), wxSize(1000,500));
-  asciiInput->Bind(wxEVT_KEY_DOWN, &MainFrame::OnAsciiKeyDown, this);
+  asciiInput->Bind(wxEVT_CHAR, &MainFrame::OnAsciiKeyDown, this);
   hexInput = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(0, 500), wxSize(1000,500));
-  clearButton = new wxButton(panel, wxID_ANY, "CLEAR", wxPoint(0, 1000), wxSize(200, 200));
-
+  hexInput->Bind(wxEVT_CHAR, &MainFrame::OnAsciiKeyDown, this);
+  clearButton = new wxButton(panel, wxID_ANY, "CLEAR", wxPoint(0, 1000), wxSize(300, 200));
+  clearButton->Bind(wxEVT_BUTTON, &MainFrame::OnClearButton, this);
+  saveButton = new wxButton(panel, wxID_ANY, "SAVE", wxPoint(350, 1000), wxSize(300,200));
+  saveButton->Bind(wxEVT_BUTTON, &MainFrame::OnSaveButton, this);
+  loadButton = new wxButton(panel, wxID_ANY, "LOAD", wxPoint(700, 1000), wxSize(300,200));
+  loadButton->Bind(wxEVT_BUTTON, &MainFrame::OnLoadButton, this);
 }
 
--- mainframe.hpp
+++ mainframe.hpp
@@ -3,6 +3,7 @@
 #include <wx/generic/grid.h>
 #include <wx/wx.h>
 
+
 class MainFrame : public wxFrame {
 public:
   MainFrame(const wxString &title);
@@ -11,9 +12,18 @@ private:
   wxPanel *panel;
   wxTextCtrl* asciiInput;
   wxTextCtrl* hexInput;
-  wxButton *clearButton;
+  wxButton* clearButton;
+  wxButton* saveButton;
+  wxButton* loadButton;
+  
 
   void OnAsciiKeyDown(wxKeyEvent& key);
+  void OnClearButton(wxCommandEvent &event);
+  void OnSaveButton(wxCommandEvent &event);
+  void OnLoadButton(wxCommandEvent &event);
+  int WriteInFile(std::string text);
+  int LoadFromFile();
+  
 };
 
 #endif // MAINFRAME_HPP
